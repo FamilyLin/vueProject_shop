c98f592e68ea8ebceee0fa757f3e3595639ef939
zhangjialin <1143753952@qq.com>
2021-07-07 22:24:10 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <router-view/>
+    <footer-guide v-show="$route.meta.showFooter == true"/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style>
+</style>
