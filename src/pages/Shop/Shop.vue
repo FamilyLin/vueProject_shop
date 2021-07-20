3c461207af95f17ca137f43c2f3366c7e6a6dcf2
zhangjialin <1143753952@qq.com>
2021-07-20 21:21:37 +0800

    feat: complete shopList

---
+++ src/pages/Shop/Shop.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <shop-heaer />
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/shop/goods">点餐</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/shop/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/shop/info">商家信息</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ShopHeaer from '../../components/ShopHeader/ShopHeaer.vue'
+
+export default {
+  components: {
+    ShopHeaer
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus"></style>
