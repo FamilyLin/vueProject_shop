dad225520be5325be4e0454bf8b5470597c2bcf5
zhangjialin <1143753952@qq.com>
2021-07-22 23:06:15 +0800

    feat: ShopGoods

--- src/pages/Shop/Shop.vue
+++ src/pages/Shop/Shop.vue
@@ -3,13 +3,16 @@
     <ShopHeader />
     <div class="tab">
       <div class="tab-item">
-        <router-link to="/shop/goods" replace>点餐</router-link>
+        <router-link to="/shop/goods"
+                     replace>点餐</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/shop/ratings" replace>评论</router-link>
+        <router-link to="/shop/ratings"
+                     replace>评论</router-link>
       </div>
       <div class="tab-item">
-        <router-link to="/shop/info" replace>商家信息</router-link>
+        <router-link to="/shop/info"
+                     replace>商家信息</router-link>
       </div>
     </div>
     <router-view></router-view>
@@ -29,4 +32,41 @@ export default {
 }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus"></style>
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl';
+
+.tab {
+  height: 40px;
+  line-height: 40px;
+  background: #fff;
+  bottom-border-1px(rgba(7, 17, 27, 0.1));
+
+  .tab-item {
+    float: left;
+    width: 33.33333%;
+    text-align: center;
+    font-size: 14px;
+    color: rgb(77, 85, 93);
+
+    a {
+      display: block;
+      position: relative;
+
+      &.router-link-active {
+        color: #02a774;
+
+        &::after {
+          content: '';
+          position: absolute;
+          left: 50%;
+          bottom: 1px;
+          width: 35px;
+          height: 2px;
+          transform: translateX(-50%);
+          background: #02a774;
+        }
+      }
+    }
+  }
+}
+</style>
